56966604c47ac9143f18354461573c37bff0163f
Thomas Flemming <thomas.flemming@usit.uio.no>
2010-03-10 11:20:26 +0100

    spec for propfind(path, :acl)

--- spec/integration/net_dav_spec.rb
+++ spec/integration/net_dav_spec.rb
@@ -84,6 +84,12 @@ describe "Net::Dav" do
     dav.exists?('/totally_unknown_file.html').should == false
   end
 
+  it "should retrieve acl" do
+    dav = Net::DAV.new("http://localhost:10080/")
+    @props = dav.propfind("/", :acl).to_s
+    @props.should match(/200 OK/i)
+  end
+
 # proppatch seems to work, but our simple webdav server don't update properties
 #   it "should alter properties on resources on webdav server" do
 #     dav = Net::DAV.new("http://localhost:10080/")
